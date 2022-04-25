2a86e0a4f0a952ef1756f0fddd6b308621fa0b16
Jackson Gomes <jacksonanderson1515@hotmail.com>
2022-04-25 15:00:20 -0300

    initial commit

---
+++ src/main/java/br/com/bookstoreapi/books/BookstoreBooksApplication.java
@@ -0,0 +1,13 @@
+package br.com.bookstoreapi.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreBooksApplication.class, args);
+	}
+
+}
